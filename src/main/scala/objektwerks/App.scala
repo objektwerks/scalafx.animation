600a12f3a4bb002b56f714ad616b51fd37be9880
objektwerks <objektwerks@runbox.com>
2025-05-06 15:17:37 -0400

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -7,5 +7,5 @@ object App extends JFXApp3:
     stage = new JFXApp3.PrimaryStage:
       scene = View().scene
       title = "Animation"
-      width = 600
-      height = 600
+      width = 300
+      height = 300
