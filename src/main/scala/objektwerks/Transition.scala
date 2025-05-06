0633b3ad1253521b95aa4a3f779451ef7cd738f0
objektwerks <objektwerks@runbox.com>
2025-05-06 15:12:34 -0400

    added fill transition

--- src/main/scala/objektwerks/Transition.scala
+++ src/main/scala/objektwerks/Transition.scala
@@ -1,7 +1,9 @@
 package objektwerks
 
-import scalafx.animation.{FadeTransition, Interpolator, Timeline}
+import scalafx.animation.{FadeTransition, FillTransition, Interpolator, Timeline}
 import scalafx.scene.Node
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Shape
 import scalafx.util.Duration
 
 object Transition:
@@ -10,6 +12,15 @@ object Transition:
     transition.setFromValue(1.0)
     transition.setToValue(0)
     transition.setInterpolator(Interpolator.Linear)
-    transition.setCycleCount(Timeline.INDEFINITE)
+    transition.setCycleCount(Timeline.Indefinite)
     transition.play()
     node
+
+  def fill(shape: Shape): Shape =
+    val transition = FillTransition(Duration.apply(5000), shape)
+    transition.setFromValue(Color.Red)
+    transition.setToValue(Color.Green)
+    transition.setInterpolator(Interpolator.Linear)
+    transition.setCycleCount(Timeline.Indefinite)
+    transition.play()
+    shape
