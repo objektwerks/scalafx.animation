ee90c636bf36b6e92b346b1d067b04ff8f5a097a
objektwerks <objektwerks@runbox.com>
2025-05-06 16:46:40 -0400

    added path to transition

--- src/main/scala/objektwerks/Transition.scala
+++ src/main/scala/objektwerks/Transition.scala
@@ -1,8 +1,8 @@
 package objektwerks
 
-import scalafx.animation.{FadeTransition, FillTransition, Interpolator, Timeline, TranslateTransition}
+import scalafx.animation.{FadeTransition, FillTransition, Interpolator, PathTransition, Timeline, TranslateTransition}
 import scalafx.scene.paint.Color
-import scalafx.scene.shape.Shape
+import scalafx.scene.shape.{Path, Shape}
 import scalafx.util.Duration
 
 /**
@@ -31,7 +31,18 @@ object Transition:
     val transition = new TranslateTransition(Duration(5000), shape)
     transition.setToX(10)
     transition.setToY(10)
-    transition.setCycleCount(Timeline.Indefinite)
     transition.setInterpolator(Interpolator.LINEAR)
+    transition.setCycleCount(Timeline.Indefinite)
+    transition.play()
+    shape
+
+  def path(shape: Shape): Shape =
+    val path = new Path()
+    path.getElements().add( MoveTo(50, 50) )
+    path.getElements().add( LineTo(250, 250) )
+
+    val transition = new PathTransition(Duration(5000), path, shape)
+    transition.setInterpolator(Interpolator.Linear)
+    transition.setCycleCount(Timeline.Indefinite)
     transition.play()
     shape
