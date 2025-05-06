0741f766b4f4548622c1d0935a0190b79a010a2c
objektwerks <objektwerks@runbox.com>
2025-05-06 12:59:23 -0400

    enhanced timeline pane

--- src/main/scala/objektwerks/TimelinePane.scala
+++ src/main/scala/objektwerks/TimelinePane.scala
@@ -1,7 +1,10 @@
 package objektwerks
 
 import scalafx.geometry.Insets
-import scalafx.scene.layout.VBox
+import scalafx.scene.layout.{Priority, VBox}
 
 final class TimelinePane extends VBox:
   padding = Insets(6)
+
+  children = List()
+  VBox.setVgrow(this, Priority.Always)
