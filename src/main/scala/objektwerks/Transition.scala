aa953469c58c7615a0817341ca1df6a3dd464dd7
objektwerks <objektwerks@runbox.com>
2025-05-06 14:42:04 -0400

    enhanced transition

--- src/main/scala/objektwerks/Transition.scala
+++ src/main/scala/objektwerks/Transition.scala
@@ -1,12 +1,12 @@
 package objektwerks
 
 import scalafx.animation.{FadeTransition, Interpolator}
-import scalafx.scene.shape.Circle
+import scalafx.scene.Node
 import scalafx.util.Duration
 
 object Transition:
-  def fade(circle: Circle): Unit =
-    val transition = FadeTransition(Duration.apply(5000), circle)
+  def fade(node: Node): Unit =
+    val transition = FadeTransition(Duration.apply(5000), node)
     transition.setFromValue(1.0)
     transition.setToValue(0)
     transition.setInterpolator(Interpolator.Linear)
