2689f1e589c37ff17a4d39bd82643d887c8c6bd8
objektwerks <objektwerks@runbox.com>
2025-05-06 14:46:59 -0400

    integrated transition with transition pane

--- src/main/scala/objektwerks/Transition.scala
+++ src/main/scala/objektwerks/Transition.scala
@@ -5,9 +5,10 @@ import scalafx.scene.Node
 import scalafx.util.Duration
 
 object Transition:
-  def fade(node: Node): Unit =
+  def fade(node: Node): Node =
     val transition = FadeTransition(Duration.apply(5000), node)
     transition.setFromValue(1.0)
     transition.setToValue(0)
     transition.setInterpolator(Interpolator.Linear)
     transition.play()
+    node
--- src/main/scala/objektwerks/TransitionPane.scala
+++ src/main/scala/objektwerks/TransitionPane.scala
@@ -1,11 +1,17 @@
 package objektwerks
 
 import scalafx.geometry.Insets
-import scalafx.scene.control.Label
-import scalafx.scene.layout.{Priority, VBox}
+import scalafx.scene.layout.{Pane, Priority, VBox}
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Circle
+
+import Transition.*
 
 final class TransitionPane extends VBox:
   padding = Insets(6)
 
-  children = List( Label("TODO") )
+  val fadePane = new Pane:
+    children = List( fade( Circle(150, 150, 20, Color.Green) ) )
+
+  children = List(fadePane)
   VBox.setVgrow(this, Priority.Always)
