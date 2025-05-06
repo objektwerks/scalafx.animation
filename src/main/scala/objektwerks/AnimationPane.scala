36e465ade7978c63a87e7fee0e683f8b4b71f9c8
objektwerks <objektwerks@runbox.com>
2025-05-06 19:52:29 -0400

    enhanced view

--- src/main/scala/objektwerks/AnimationPane.scala
+++ src/main/scala/objektwerks/AnimationPane.scala
@@ -1,19 +1,10 @@
 package objektwerks
 
 import scalafx.geometry.Insets
-import scalafx.scene.control.{Tab, TabPane}
 import scalafx.scene.layout.{Priority, VBox}
 
 final class AnimationPane extends VBox:
   padding = Insets(6)
 
-  val transitionTab = new Tab:
-    text = "Animation"
-    closable = false
-    content = TransitionPane()
-
-  val tabPane = new TabPane:
-    tabs = List(transitionTab)
-
-  children = List(tabPane)
+  children = List( TransitionPane() )
   VBox.setVgrow(this, Priority.Always)
