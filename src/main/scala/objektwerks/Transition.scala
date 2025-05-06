f6f274696a59d6358f3433e214bb9d0d0ae73303
objektwerks <objektwerks@runbox.com>
2025-05-06 16:40:17 -0400

    added scaladoc to transition

--- src/main/scala/objektwerks/Transition.scala
+++ src/main/scala/objektwerks/Transition.scala
@@ -5,6 +5,10 @@ import scalafx.scene.paint.Color
 import scalafx.scene.shape.Shape
 import scalafx.util.Duration
 
+/**
+  * See: https://dev.java/learn/javafx-animations/
+  */
+ */
 object Transition:
   def fade(shape: Shape): Shape =
     val transition = FadeTransition(Duration(millis = 5000), shape)
