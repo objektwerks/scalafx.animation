5f619a78c6165db35950d037b4af94c15b9a0010
objektwerks <objektwerks@runbox.com>
2025-05-06 19:19:57 -0400

    enhanced timeline

--- src/main/scala/objektwerks/Timeline.scala
+++ src/main/scala/objektwerks/Timeline.scala
@@ -1,6 +1,16 @@
 package objektwerks
 
+import scalafx.animation.{KeyFrame, KeyValue, Timeline}
+import scalafx.scene.shape.Shape
+import scalafx.util.Duration
+
 /**
   * See: https://dev.java/learn/javafx-animations/
   */
-object Timeline
+object Timeline:
+  def timeline(shape: Shape): Shape =
+    val translate = KeyValue(shape.translateXProperty(), 10)
+    val frames = KeyFrame(Duration(millis = 5000), values = Set(translate))
+    val timeline = Timeline( List(frames) )
+    timeline.setCycleCount(Timeline.Indefinite)
+    timeline.play()
