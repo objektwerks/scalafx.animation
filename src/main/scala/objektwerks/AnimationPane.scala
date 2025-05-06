8ca8f1b818874fb807a23e1816ba09d62df6d549
objektwerks <objektwerks@runbox.com>
2025-05-06 12:49:52 -0400

    enhanced animation pane

--- src/main/scala/objektwerks/AnimationPane.scala
+++ src/main/scala/objektwerks/AnimationPane.scala
@@ -1,7 +1,13 @@
 package objektwerks
 
 import scalafx.geometry.Insets
+import scalafx.scene.control.{Label, Tab}
 import scalafx.scene.layout.VBox
 
 final class AnimationPane extends VBox:
   padding = Insets(3)
+
+  val transitionTab = new Tab:
+    text = "Transition"
+    closable = false
+    content = Label("TODO")
