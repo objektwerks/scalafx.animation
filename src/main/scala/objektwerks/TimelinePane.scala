7864cd7f5f17e0eb5714bbdb2dcd009b304b166f
objektwerks <objektwerks@runbox.com>
2025-05-06 19:31:14 -0400

    enhanced timeline pane

--- src/main/scala/objektwerks/TimelinePane.scala
+++ src/main/scala/objektwerks/TimelinePane.scala
@@ -2,10 +2,18 @@ package objektwerks
 
 import scalafx.geometry.Insets
 import scalafx.scene.control.Label
-import scalafx.scene.layout.{Priority, VBox}
+import scalafx.scene.layout.{Pane, Priority, VBox}
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Circle
+
+import Timeline.*
 
 final class TimelinePane extends VBox:
   padding = Insets(6)
+
+  val timeLineLabel = Label("Time Line:")
+  val timeLinePane = new Pane:
+    children = List( timeline( Circle(10, 10, 10, Color.Red) ) )
 
   children = List( Label("TODO") )
   VBox.setVgrow(this, Priority.Always)
