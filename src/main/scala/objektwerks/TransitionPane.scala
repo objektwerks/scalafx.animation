0fc15b453cf89f3f03765f4317decd2dab40af88
objektwerks <objektwerks@runbox.com>
2025-05-06 14:53:29 -0400

    enhanced transition pane

--- src/main/scala/objektwerks/TransitionPane.scala
+++ src/main/scala/objektwerks/TransitionPane.scala
@@ -1,7 +1,8 @@
 package objektwerks
 
 import scalafx.geometry.Insets
-import scalafx.scene.layout.{Pane, Priority, VBox}
+import scalafx.scene.control.Label
+import scalafx.scene.layout.{Priority, VBox}
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.Circle
 
@@ -10,8 +11,9 @@ import Transition.*
 final class TransitionPane extends VBox:
   padding = Insets(6)
 
-  val fadePane = new Pane:
-    children = List( fade( Circle(100, 100, 20, Color.Green) ) )
+  val fadeLabel = Label("fade:")
+  val fadePane = new VBox:
+    children = List( fadeLabel, fade( Circle(50, 50, 10, Color.Red) ) )
 
   children = List(fadePane)
   VBox.setVgrow(this, Priority.Always)
