8fb5eb848dee256954013efe830872cebb0cb46a
objektwerks <objektwerks@runbox.com>
2025-05-06 20:45:20 -0400

    refactored control grid

--- src/main/scala/objektwerks/ControlGrid.scala
+++ src/main/scala/objektwerks/ControlGrid.scala
@@ -3,14 +3,12 @@ package objektwerks
 import scalafx.geometry.Insets
 import scalafx.scene.Node
 import scalafx.scene.control.Label
-import scalafx.scene.layout.{ColumnConstraints, GridPane}
+import scalafx.scene.layout.GridPane
 
-final class ControlGrid(controls: List[(Label, Node)],
-                        columnConstraints: List[ColumnConstraints] = List.empty[ColumnConstraints]) extends GridPane:
+final class ControlGrid(controls: List[(Label, Node)]) extends GridPane:
   hgap = 12
   vgap = 12
   padding = Insets(top = 12, right = 12, bottom = 12, left = 12)
-  columnConstraints = columnConstraints
 
   var row = 0
   for ((label, node) <- controls)
